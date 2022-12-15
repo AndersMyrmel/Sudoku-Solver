804d3be4709c35dc3c6d46a5075a816a71db76f8
Anders <andersmyrmel@gmail.com>
2022-12-15 16:37:14 +0100

    clear cache

--- tempCodeRunnerFile.hs
+++
@@ -1,9 +0,0 @@
-solve' :: [[Int]] -> Maybe [[Int]]
-solve' board
-  | not (null zeros) = do
-      let (r, c) = head zeros
-      let possibilities = [x | x <- [1..9], x `notElem` (getRow board r) ++ (getCol board c) ++ (getBlock board r c)]
-      foldM solve' (setAtIndex board (r, c) x) possibilities
-        where let x = head possibilities
-  | otherwise = Just board
-  where zeros = [(r, c) | r <- [0..8], c <- [0..8], (getIndex board r c) == 0]
